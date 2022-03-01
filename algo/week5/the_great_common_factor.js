55cbd73d4e34c2e37d8d66a71a0bf67151d07e1f
Lisa <lb.broadhead@gmail.com>
2022-03-01 10:57:22 -0600

    algos

--- algo/week5/the_great_common_factor.js
+++ algo/week5/the_great_common_factor.js
@@ -1,23 +1,25 @@
 function recursiveGreatestCommonFactor(val1,val2){
-    var mod = val1 % val2;
-    console.log(` mod : ${mod}`)
+    // if val1 is bigger
+    // if(val1 > val2){
+    //     var temp = val1;
+    //     val1 = val2;
+    //     val2 = temp;
+    // }
 
+    var mod = val1 % val2;
     if(val1 % val2 == 2){
         val1 -= mod;
         val2 -= mod;
     }
 
-    console.log(val1)
-    console.log(val2)
-
     if(val1 === val2 || mod == 0 ){
-        return val1;
+        return val2;
     }else if(val1 > val2){
         val1 -= val2
-        return recursiveGreatestCommonFactor(val1,val2)
+        return recursiveGreatestCommonFactor(val1,val2);
     }else if(val1 < val2){
         val2 -= val1
-        return recursiveGreatestCommonFactor(val1,val2)
+        return recursiveGreatestCommonFactor(val1,val2);
     }
 }
 
@@ -25,9 +27,17 @@ function recursiveGreatestCommonFactor(val1,val2){
 // console.log(recursiveGreatestCommonFactor(50,180)); // 10
 // console.log(recursiveGreatestCommonFactor(7,35)); // 7
 // console.log(recursiveGreatestCommonFactor(65,95)); // 5
-// console.log(recursiveGreatestCommonFactor(123456,987654)); // 6
-console.log(recursiveGreatestCommonFactor(102,1000)); // 2
+console.log(recursiveGreatestCommonFactor(123456,987654)); // 6
+// console.log(recursiveGreatestCommonFactor(102,1000)); // 2
 // console.log(recursiveGreatestCommonFactor(7,13)); // 1
+
+// console.log(recursiveGreatestCommonFactor(987654,123456)); // 6
+// console.log(recursiveGreatestCommonFactor(1000,102)); // 2
+// console.log(recursiveGreatestCommonFactor(13,7)); // 1
+
+
+
+
 
 
 // ANSWER
